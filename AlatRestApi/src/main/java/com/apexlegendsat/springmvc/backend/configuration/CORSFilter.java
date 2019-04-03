c3cd0ab71da08158f053454ab947bbb4eb942161
Joseph Slagle <SteeledSlagle13@gmail.com>
2019-04-03 09:54:40 -0500

    Moved to new EWorkspace - Changing access address

--- AlatRestApi/src/main/java/com/apexlegendsat/springmvc/backend/configuration/CORSFilter.java
+++ AlatRestApi/src/main/java/com/apexlegendsat/springmvc/backend/configuration/CORSFilter.java
@@ -15,7 +15,7 @@ public class CORSFilter implements Filter {
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "http://localhost:8085");
+		response.setHeader("Access-Control-Allow-Origin", "http://localhost:8086");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "X-requested-with, Content-Type");
